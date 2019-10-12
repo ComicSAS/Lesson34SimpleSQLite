a3d3a75dd759a4b359f4f070c20dc41394542a26
Sukhovolskij.Andrew <sukhovolskijandrewoid@gmail.com>
2019-10-12 20:30:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson34simplesqlite/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.lesson34simplesqlite;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.lesson34simplesqlite.utils.DBHelper;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    final String LOG_TAG = "Log";
+
+    Button btnAdd, btnClear, btnRead;
+
+    EditText etName, etEmail;
+
+    DBHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(this);
+
+        btnRead = (Button) findViewById(R.id.btnRead);
+        btnRead.setOnClickListener(this);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+
+        // создаем объект для создания и управления версиями БД
+        dbHelper = new DBHelper(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // создаем объект для данных
+        ContentValues cv = new ContentValues();
+
+        // получаем данные из полей ввода
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+
+        // подключаемся к БД
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+                // подготовим данные для вставки в виде пар: наименование столбца - значение
+                cv.put("name", name);
+                cv.put("email", email);
+
+                // вставляем запись и получаем ее ID
+                long rowID = db.insert("mytable", null, cv);
+                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
+                break;
+
+            case R.id.btnRead:
+                Log.d(LOG_TAG, "--- Rows in mytable: ---");
+                // делаем запрос всех данных из таблицы mytable, получаем Cursor
+                Cursor cursor = db.query("mytable", null, null, null, null, null, null);
+                // ставим позицию курсора на первую строку выборки
+                // если в выборке нет строк, вернется false
+                if (cursor.moveToFirst()) {
+                    // определяем номера столбцов по имени в выборке
+                    int idIndex = cursor.getColumnIndex("id");
+                    int nameIndex = cursor.getColumnIndex("name");
+                    int emailIndex = cursor.getColumnIndex("email");
+
+do {
+    // получаем значения по номерам столбцов и пишем все в лог
+    Log.d(LOG_TAG,
+            "ID = " + cursor.getInt(idIndex) +
+                    ", name = " + cursor.getString(nameIndex) +
+                    ", email = " + cursor.getString(emailIndex));
+    // переход на следующую строку
+    // а если следующей нет (текущая - последняя), то false - выходим из цикла
+}while (cursor.moveToNext());
+                } else
+                    Log.d(LOG_TAG, "0 rows");
+                cursor.close();
+                break;
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "--- Clear mytable: ---");
+                // удаляем все записи
+                int clearCount = db.delete("mytable",null,null);
+                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+                break;
+        }
+        // закрываем подключение к БД
+        dbHelper.close();
+    }
+}
